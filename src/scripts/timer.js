5a44030d1d1a2d89c8c8d91809f9e9aa70e0dae5
Sergey <adamas34@gmail.com>
2019-05-19 21:00:56 +0300

    fix4

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -1,16 +1,19 @@
 const year = new Date().getFullYear();
 
-const twentyOfMay = new Date(year, 4, 22).getTime(); // 4 - это май, тк 0 - январь. 22 - это 21 мая
+const timerDay = new Date(year, 4, 24).getTime(); // 4 - это май, тк 0 - январь. 20 - это 20 мая
 
 let timer = setInterval(() => {
 
   const today = new Date().getTime(); // дата на сегодня
 
-  const diff = twentyOfMay - today; // разница между 20.05.2019 и сегодня
+  const diff = timerDay - today; // разница между 24.05.2019 и сегодня
 
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)*days);
+  // let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  
+  let hours = Math.floor(diff / (1000 * 60 * 60));
+  
   let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+
   let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
   document.querySelector('.timer-hour').textContent = String(hours).padStart(2, '0');
